7d7e1122c86eec090b3bcc94d12da5f8f0cff100
Md Saddam Hossain <shishir.cse.hstu@gmail.com>
2019-04-09 22:53:34 +0600

    Create math.h

---
+++ math.h
@@ -0,0 +1,54 @@
+C++ abs() 	returns absolute value of an argument
+C++ acos() 	Returns Inverse cosine a Number
+C++ acosh() 	returns hyperbolic cosine of a number
+C++ asin() 	Returns Inverse Sine a Number
+C++ asinh() 	returns arc hyperbolic sine of a number
+C++ atan() 	Returns Inverse tangent a Number
+C++ atan2() 	Returns Inverse Tangent of a Coordinate
+C++ atanh() 	returns arc hyperbolic tangent of a number
+C++ cbrt() 	Computes Cube Root of a Number
+C++ ceil() 	Return ceiling value of number
+C++ copysign() 	returns num with value of first and sign of second
+C++ cos() 	Returns Cosine of the Argument
+C++ cosh() 	Returns Hyperbolic Cosine of an Angle
+C++ exp() 	returns exponential (e) raised to a number
+C++ exp2() 	Returns 2 raised to a Number
+C++ expm1() 	Returns e raised to Power Minus 1
+C++ fabs() 	returns absolute value of argument
+C++ fdim() 	Returns Positive Different Between Arguments
+C++ floor() 	Returns floor value of decimal number
+C++ fma() 	Returns Fused Multiply–Accumulate
+C++ fmax() 	returns largest among two arguments passed
+C++ fmin() 	returns smallest among two given arguments
+C++ fmod() 	Computes floating point remainder of division
+C++ frexp() 	breaks float to its binary significand
+C++ hypot() 	Returns Square Root of sum of square of Arguments
+C++ ilogb() 	returns integral part of logarithm of |x|
+C++ ldexp() 	returns product of x and 2 raised to the power e
+C++ llrint() 	Rounds argument using current rounding mode
+C++ llround() 	Rounds argument to nearest long long int value
+C++ log() 	Returns Natural Logarithm of a Number
+C++ log10() 	Returns Base 10 Logarithm of a Number
+C++ log1p() 	returns natural logarithm of x+1.
+C++ log2() 	returns base2 logarithm of a number
+C++ logb() 	returns logarithm of |x|
+C++ lrint() 	Rounds argument using current rounding mode
+C++ lround() 	Returns the long int value nearest to the argument
+C++ modf() 	Breaks Number Into Integral and Fractional Part
+C++ nan() 	returns a quiet NaN value
+C++ nearbyint() 	Rounds argument to using current rounding mode
+C++ nextafter() 	returns next value after x in direction of y
+C++ nexttoward() 	returns next value after x in direction of y
+C++ pow() 	Computes Power a Number
+C++ remainder() 	Returns remainder of x/y
+C++ remquo() 	Computer remainder and stores quotient of x/y
+C++ rint() 	Rounds argument using current rounding mode
+C++ round() 	Returns integral value nearest to argument
+C++ scalbln() 	Scales x by FLT_RADIX to the power n
+C++ scalbn() 	Scales x by FLT_RADIX to the power n
+C++ sin() 	Returns Sine of the Argument
+C++ sinh() 	returns hyperbolic sine of an angle
+C++ sqrt() 	Computes Square Root of A Number
+C++ tan() 	Returns Tangent of the Argument
+C++ tanh() 	returns hyperbolic tangent of an angle
+C++ trunc() 	Truncates the demical part of a number 
